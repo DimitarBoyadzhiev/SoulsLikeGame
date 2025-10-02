d43e9366b73e776e0fa590ac6e0fbcc2b9d9d102
Dimitar Boyadzhiev <dimitar.boyadzhiev02@gmail.com>
2025-10-02 14:54:59 +0300

    feat: Lockon feature

--- Source/SoulsLikeGame/Private/Combat/LockonComponent.cpp
+++ Source/SoulsLikeGame/Private/Combat/LockonComponent.cpp
@@ -2,6 +2,11 @@
 
 
 #include "Combat/LockonComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Math/Vector.h"
 
 // Sets default values for this component's properties
 ULockonComponent::ULockonComponent()
@@ -19,16 +24,96 @@ void ULockonComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	OwnerRef = GetOwner<ACharacter>();
+	Controller = GetWorld()->GetFirstPlayerController();
+	MovementComp = OwnerRef->GetCharacterMovement();
+	SpringArmComp = OwnerRef->FindComponentByClass<USpringArmComponent>();
 }
-
 
 // Called every frame
 void ULockonComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	if (!IsValid(CurrentTargetActor)) { return; }
+
+	FVector CurrentLocation{ OwnerRef->GetActorLocation() };
+	FVector TargetLocation{ CurrentTargetActor->GetActorLocation() };
+
+	// Check if distance between player and target is too far
+	if (FVector::Distance(CurrentLocation, TargetLocation) >= BreakDistance)
+	{
+		EndLockon();
+		return;
+	}
+
+	TargetLocation.Z -= 125;
+
+	FRotator NewRotation{ UKismetMathLibrary::FindLookAtRotation(
+		CurrentLocation, TargetLocation
+	) };
+
+	Controller->SetControlRotation(NewRotation);
 }
+
+void ULockonComponent::StartLockon(float Radius)
+{
+	FHitResult OutResult;
+	FVector CurrentLocation{ OwnerRef->GetActorLocation() };
+	FCollisionShape Sphere{ FCollisionShape::MakeSphere(Radius) };
+	FCollisionQueryParams IgnoreParams{
+		FName{ TEXT("Ignore Collision Params")},
+		false,
+		OwnerRef
+	};
+
+	bool bHasFoundTarget = { GetWorld()->SweepSingleByChannel(
+		OutResult,
+		CurrentLocation,
+		CurrentLocation,
+		FQuat::Identity,
+		ECollisionChannel::ECC_GameTraceChannel1,
+		Sphere,
+		IgnoreParams
+	) };
+
+	if (!bHasFoundTarget) { return; }
+
+	CurrentTargetActor = OutResult.GetActor();
+
+	// Disable mouse look
+	Controller->SetIgnoreLookInput(true);
+	// Disable rotation to movement
+	MovementComp->bOrientRotationToMovement = false;
+	MovementComp->bUseControllerDesiredRotation = true;
+
+	SpringArmComp->TargetOffset = FVector{ 0.0, 0.0, 100.0 };
+
+}
+
+void ULockonComponent::EndLockon()
+{
+	CurrentTargetActor = nullptr;
+
+	MovementComp->bOrientRotationToMovement = true;
+	MovementComp->bUseControllerDesiredRotation = false;
+	SpringArmComp->TargetOffset = FVector::ZeroVector;
+
+	Controller->ResetIgnoreLookInput();
+}
+
+void ULockonComponent::ToggleLockon(float Radius)
+{
+	if (IsValid(CurrentTargetActor))
+	{
+		EndLockon();
+	}
+	else
+	{
+		StartLockon(Radius);
+	}
+}
+
+
+
 
--- Source/SoulsLikeGame/Public/Combat/LockonComponent.h
+++ Source/SoulsLikeGame/Public/Combat/LockonComponent.h
@@ -12,13 +12,34 @@ class SOULSLIKEGAME_API ULockonComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+	ACharacter* OwnerRef;
+
+	APlayerController* Controller;
+
+	class UCharacterMovementComponent* MovementComp;
+
+	class USpringArmComponent* SpringArmComp;
+
 public:	
 	// Sets default values for this component's properties
 	ULockonComponent();
 
+	AActor* CurrentTargetActor;
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	void StartLockon(float Radius = 750.0f);
+
+	void EndLockon();
+
+	UFUNCTION(BlueprintCallable)
+	void ToggleLockon(float Radius = 750.0f);
+
+	UPROPERTY(EditAnywhere)
+	double BreakDistance{ 1000 };
 
 public:	
 	// Called every frame
